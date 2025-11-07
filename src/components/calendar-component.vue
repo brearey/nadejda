7265dcea22cdfe14547908f9450a55ad459c320e
lorriant <brearey88@yandex.ru>
2025-11-07 10:51:29 +0900

    created calendar component

---
+++ src/components/calendar-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Hello {{ like }}</div>
+  <button v-on:click="addLike">Like</button>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      like: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.like++
+    },
+  },
+}
+</script>
