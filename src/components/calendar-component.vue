04125fd9b534a3378f1ef4cc0345ef120405561a
lorriant <brearey88@yandex.ru>
2025-11-07 11:16:27 +0900

    current date done, eng and rus versions done

--- src/components/calendar-component.vue
+++ src/components/calendar-component.vue
@@ -2,15 +2,15 @@
   <div class="container">
     <div class="header">
       <button>Previous month</button>
-      <span>November 2025</span>
+      <span>{{ monthsNames.english[currentMonthIndex] }} {{ currentYear }}</span>
       <button>Next month</button>
     </div>
     <div class="grid">
       <div class="week-days">
-        <div v-for="weekDay in weekDays" :key="weekDay">{{ weekDay }}</div>
+        <div v-for="weekDay in weekDaysNames.english" :key="weekDay">{{ weekDay }}</div>
       </div>
       <div class="month-days">
-        <div v-for="(monthDay, idx) in monthDays" :key="idx">
+        <div v-for="(monthDay, idx) in monthDaysNumbers" :key="idx">
           {{ monthDay }}
         </div>
       </div>
@@ -21,9 +21,61 @@
 <script lang="ts">
 export default {
   data() {
+    const currentDate = new Date()
     return {
-      weekDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      monthDays: [
+      currentMonthIndex: currentDate.getMonth(),
+      currentYear: currentDate.getFullYear(),
+      weekDaysNames: {
+        english: [
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday',
+        ],
+        russian: [
+          'Понедельник',
+          'Вторник',
+          'Среда',
+          'Четверг',
+          'Пятница',
+          'Суббота',
+          'Воскресенье',
+        ]
+      },
+      monthsNames: {
+        english: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
+        russian: [
+          'Январь',
+          'Февраль',
+          'Март',
+          'Апрель',
+          'Май',
+          'Июнь',
+          'Июль',
+          'Август',
+          'Сентябрь',
+          'Октябрь',
+          'Ноябрь',
+          'Декабрь',
+        ],
+      },
+      monthDaysNumbers: [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
         26, 27, 28, 29, 30,
       ],
